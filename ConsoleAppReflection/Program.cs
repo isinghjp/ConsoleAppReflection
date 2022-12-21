c2abeb1351454099f2b38302020130534393ed30
imjays <jpkumar541@gmail.com>
2022-12-21 12:56:19 +0530

    Add project files.

---
+++ ConsoleAppReflection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleAppReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string asmPath = "";
+            Assembly assembly = Assembly.LoadFrom(asmPath);    
+        }
+        public static void ShowAssemblyDetails(Assembly assembly) 
+        {
+            Console.WriteLine($"Full Name: {assembly.FullName}\n GAC: {assembly.GlobalAssemblyCache}\n Location: {assembly.Location}");
+
+        }
+    }
+}
